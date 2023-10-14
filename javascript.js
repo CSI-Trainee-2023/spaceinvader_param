5a84e7a2f37005b845b684c0e13cbc06e43fc9e9
params19 <paramsrivastava9@gmail.com>
2023-10-14 23:48:11 +0530

    Left Right Moving

--- javascript.js
+++ javascript.js
@@ -22,17 +22,53 @@ class Player{
         }
     }
     draw(){
-        if(this.image)
         c.drawImage(this.image,this.position.x,this.position.y,this.width,this.height)
     }
+PaymentRequestUpdateEvent(){
+    if(this.image){
+    this.draw()
+this.position.x+=this.velocity.x
+}
+}
 }
 const player=new Player()
-player.draw()
-
+const keys={
+    a:{
+        pressed :false
+    },
+    d:{
+        pressed :false
+    },
+    space :{
+        pressed :false
+    }
+}
 function animate(){
     requestAnimationFrame(animate)
     c.fillStyle='black'
     c.fillRect(0,0,canvas.width,canvas.height)
-    player.draw()
+    player.PaymentRequestUpdateEvent()
+
+    if(keys.a.pressed) {
+        player.velocity.x=-5
+    }else {
+        player.velocity.x=0
+    }
 }
 animate()
+
+addEventListener('keydown',({key}) => {
+    switch(key){
+        case 'a':
+            console.log('left')
+            keys.a.pressed=true
+            break
+        case 'd':
+            console.log('right')
+            keys.a.pressed=true
+            break
+        case ' ':
+            console.log('space')
+            break
+    }
+})
